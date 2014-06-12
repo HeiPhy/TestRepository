8ba30b750534fb4e3f3c9dee3c7bb80f3178e648
Jan Heider <hei14011@spengergasse.at>
2014-06-12 23:54:02 +0200

    Eine andere(verschieden von der vorhherigen auf dem anderen branch)  zeile geändert

--- src/MyScanner.java
+++ src/MyScanner.java
@@ -7,7 +7,7 @@ public class MyScanner {
 
         public static void main(String[] args) {
             Scanner s = new Scanner(System.in);
-            System.out.println("Willkommen zum Testprogramm!");
+            System.out.println("Herzlich Willkommen zum Testprogramm!");
             System.out.println("Bitte geben Sie Ihren Vorname ein:");
             String vorname = s.nextLine();
             System.out.println("Bitte geben Sie Ihren Nachname ein:");
