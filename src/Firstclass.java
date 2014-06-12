897291aac3a13b5f33b32fbcb31428f587dedfe5
Jan Heider <hei14011@spengergasse.at>
2014-06-13 00:24:27 +0200

    Zwei neue Klassen hinzugefuegt

---
+++ src/Firstclass.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Samir on 13.06.14.
+ */
+public class Firstclass {
+
+    public static void main(String[] args) {
+        System.out.println("Test")
+    }
+
+}
+
+
---
+++ src/SecondClass.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Samir on 13.06.14.
+ */
+public class SecondClass {
+
+
+    public static void main(String[] args) {
+        System.out.println("Test2")
+    }
+
+}
